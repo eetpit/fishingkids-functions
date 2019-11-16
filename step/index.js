f5e3e5937d63d275cf842ace4bf3589afdd6cb47
Eetu Pitkänen <eetu.pitkanen@cgi.com>
2019-11-16 14:44:54 +0200

    Fill placeholders

--- step/index.js
+++ step/index.js
@@ -1,47 +1,53 @@
 module.exports = async function (context, req) {
-    const currentStepNumber = +req.body.currentStep
-    const chosenAnswerNumber = +req.body.chosenAnswer
-    const manualEntry = req.body.manualEntry
+    try {
+        const currentStepNumber = +req.body.currentStep
+        const chosenAnswerNumber = +req.body.chosenAnswer
+        const manualEntry = req.body.manualEntry
 
-    if (currentStepNumber === null || currentStepNumber === undefined) {
-        context.res = {
-            status: 400,
-            body: 'Missing required params'
+        if (currentStepNumber === null || currentStepNumber === undefined) {
+            context.res = {
+                status: 400,
+                body: 'Missing required param currentStepNumber'
+            }
+
+            return
         }
 
-        return
-    }
+        const nextStep = findNextStep(context, currentStepNumber, chosenAnswerNumber, manualEntry)
+        context.res = {
+            body: `${JSON.stringify(nextStep)}`
+        }
 
+    } catch (error) {
+        context.log(`Caught error: ${JSON.stringify(error)}`)
+        context.res = {
+            status: 404,
+            body: `Step not found with params ${req.body.currentStep} ${req.body.chosenAnswer} ${req.body.manualEntry}`
+        }
+    }
+}
+
+findNextStep = (context, currentStepNumber, chosenAnswerNumber, manualEntry) => {
     const steps = context.bindings.steps
 
     if (currentStepNumber === 0) {
-        const firstStep = steps.find(item => {
-            return item.step === 1
+        const firstStep = steps.find(item => item.step === 1)
+        return firstStep
+    }
+
+    const currentStep = steps.find(item => item.step === currentStepNumber)
+    const currentAnswerOptions = currentStep.answerOptions
+
+    if (currentAnswerOptions) {
+        const chosenAnswer = currentAnswerOptions[chosenAnswerNumber]
+        const nextStep = steps.find(item => item.step === chosenAnswer.nextStep)
+
+        nextStep.messages = nextStep.messages.map(item => {
+            return item.message.replace("%placeholder%", manualEntry ? manualEntry : "Anonymous")
         })
 
-        context.res = {
-            body: `${JSON.stringify(firstStep)}`
-        }
-
-        return
+        return nextStep
     }
 
-    const currentStep = steps.find(item => {
-        // context.log(`Finding current step: ${JSON.stringify(item)} \n\n`)
-        return item.step === currentStepNumber
-    })
-
-    const currentStepAnsweredMessage = currentStep.messages.find(item => item.answerOptions)
-    const chosenAnswer = currentStepAnsweredMessage.answerOptions[chosenAnswerNumber]
-
-    const nextStep = steps.find(item => {
-        context.log(`Finding next step: ${JSON.stringify(item)} \n\n`)
-        return item.step === chosenAnswer.nextStep
-    })
-
-    context.log(`Returning step: ${JSON.stringify(nextStep)} \n\n`)
-
-    context.res = {
-        body: `${JSON.stringify(nextStep)}`
-    }
+    return { step: 99, messages: [{ message: "Thanks for playing!" }] }
 }
